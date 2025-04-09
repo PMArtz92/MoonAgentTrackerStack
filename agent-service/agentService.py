f2a023fdcf2449007083dcd605c40342ff7d9407
Pasan Ranathunga <pasan@avatarin.com>
2025-04-10 02:51:12 +0900

    Adding service files

---
+++ agent-service/agentService.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Hardcoded patient data
+agents = [
+    {
+        "agent_code": 1,
+        "name": "John Doe",
+        "age": 30,
+        "gender": "Male",
+        "products":["Life Insurance", "Health Insurance"],
+     },
+    {
+        "patient_id": 2,
+        "name": "Jane Smith",
+        "age": 25,
+        "gender": "Female",       
+        "products":["Life Insurance", "Health Insurance", "Car Insurance"],
+    }
+]
+
+@app.route("/agents/", methods=["GET"])
+def get_agents():
+    return jsonify(agents)
+
+@app.route("/agents/<int:agent_code>", methods=["GET"])
+def get_agent(agent_code: int):
+    agent = next((a for a in agents if a["agent_code"] == agent_code), None)
+    if agent:
+        return jsonify(agent)
+    return jsonify({"error": "Agent not found"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
